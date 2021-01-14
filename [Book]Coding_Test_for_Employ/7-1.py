ae3d00cfc3df42b26c8856b88f6bf9b05656d374
양 원석 <bennyws@naver.com>
2021-01-14 14:33:36 +0000

    # 7-1 재귀 함수로 구현한 이진 탐색

---
+++ [Book]Coding_Test_for_Employ/7-1.py
@@ -0,0 +1,29 @@
+# Solved on 2021. 01. 14.
+# 7-1 재귀 함수로 구현한 이진 탐색
+
+import sys
+input = sys.stdin.readline
+
+
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid - 1)
+    else:
+        return binary_search(array, target, mid + 1, end)
+
+
+n, target = list(map(int, input().split()))
+array = list(map(int, input().split()))
+
+result = binary_search(array, target, 0, n - 1)
+if result == None:
+    print("원소가 존재하지 않습니다.")
+else:
+    print(result + 1)
+
