54c734048abea82af6828cd1c60ddca25bc22cb0
gemstoneyang <bennyws99@gmail.com>
2021-08-29 05:22:47 +0000

    # 4153 직각삼각형

--- Etc/4153.py
+++ Etc/4153.py
@@ -1,17 +1,17 @@
-# Solved on 2021. 04. 13.
+# Solved on 2021. 08. 29.
 # 4153 직각삼각형
 
 import sys
 input = sys.stdin.readline
 
 while True:
-    a = list(map(int, input().split()))
-    a.sort()
+    nums = list(map(int, input().split()))
+    nums = sorted(nums)
 
-    if a[2] == 0:
+    if nums[0] == 0 and nums[1] == 0 and nums[2] == 0:
         break
 
-    if a[0]*a[0] + a[1]*a[1] == a[2]*a[2]:
+    if pow(nums[0], 2) + pow(nums[1], 2) == pow(nums[2], 2):
         print("right")
     else:
         print("wrong")
