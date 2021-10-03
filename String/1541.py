fd76db30ad35df88f4d18e05c5de966427e2d09f
gemstoneyang <bennyws@naver.com>
2021-10-03 07:49:46 +0000

    # 1541 잃어버린 괄호

--- String/1541.py
+++ String/1541.py
@@ -4,35 +4,14 @@
 import sys
 input = sys.stdin.readline
 
-string = input().strip()
-num = []
-label = []
+string = input().split('-')
 
-start = 0
-for i in range(len(string)):
-    if string[i] == '+':
-        num.append(int(string[start:i]))
-        label.append('+')
-        start = i+1
-    if string[i] == '-':
-        num.append(int(string[start:i]))
-        label.append('-')
-        start = i+1
+print(string)
 
-num.append(int(string[start:len(string)]))
 
-a = b = 0
-a += num[0]
-minus = False
 
-for i in range(len(label)):
-    if minus is True:
-        b += num[i+1]
-    else:
-        if label[i] == '-':
-            minus = True
-            b += num[i+1]
-        else:
-            a += num[i+1]
 
-print(a-b)
+
+
+
+
