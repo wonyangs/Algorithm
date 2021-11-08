7dad1e1154878b4849b68c13b0d9b086955c9657
gemstoneyang <bennyws99@gmail.com>
2021-11-08 09:58:34 +0000

    # 5338 마이크로소프트 로고

---
+++ Etc/5338.py
@@ -0,0 +1,11 @@
+# Solved on 2021. 11. 08.
+# 5338 마이크로소프트 로고
+
+import sys
+input = sys.stdin.readline
+
+print('       _.-;;-._')
+print("'-..-'|   ||   |")
+print("'-..-'|_.-;;-._|")
+print("'-..-'|   ||   |")
+print("'-..-'|_.-''-._|")
