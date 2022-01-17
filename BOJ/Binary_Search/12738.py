d65a956dbb90a5d24a5008e4a028f6a90355d33b
gemstoneyang <bennyws99@gmail.com>
2022-01-17 07:35:27 +0000

    # 12738 가장 긴 증가하는 부분 수열 3

---
+++ BOJ/Binary_Search/12738.py
@@ -0,0 +1,29 @@
+# Solved on 2022. 1. 17.
+# 12738 가장 긴 증가하는 부분 수열 3
+
+import sys
+input = sys.stdin.readline
+
+
+def binarysearch(lis, num):
+    start, end = 0, len(lis)
+    while start < end:
+        mid = (start+end) // 2
+        if lis[mid] < num:
+            start = mid + 1
+        else:
+            end = mid
+    return end
+
+
+N = int(input())
+
+arr = [*map(int, input().split())]
+lis = [arr[0]]
+for i in range(1, N):
+    if lis[-1] < arr[i]:
+        lis.append(arr[i])
+    else:
+        idx = binarysearch(lis, arr[i])
+        lis[idx] = arr[i]
+print(len(lis))
