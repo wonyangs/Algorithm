9637235ae956a08595728bb2d8c5332c17a5a440
gemstoneyang <bennyws99@gmail.com>
2022-04-08 08:54:13 +0900

    10926.java

---
+++ JAVA/10926.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String a = br.readLine();
+        System.out.println(a+"??!");
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();        
+    }
+}
