08d043bed8e0e51b4d40be76f0ff9f8c6eb75aa7
gemstoneyang <bennyws99@gmail.com>
2022-04-19 11:01:29 +0900

    11021.java

---
+++ JAVA/BOJ/11021.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; ++i) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.printf("Case #%d: %d\n", i+1, a+b);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
