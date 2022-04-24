61d96eab9ee5fc92bce4a88e2956534619b534d7
gemstoneyang <bennyws99@gmail.com>
2022-04-24 09:09:53 +0900

    10872.java

---
+++ JAVA/BOJ/10872.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+                
+        System.out.println(pibo(n));
+    }
+
+    public static int pibo(int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        return pibo(n-1) * n;
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
