2487380eacbc1d8d5b36b21bfadde6435acd4566
gemstoneyang <bennyws99@gmail.com>
2022-05-03 10:53:45 +0900

    15552.java

---
+++ JAVA/BOJ/15552.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; ++i) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write((a+b)+"\n");
+        }
+        
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
