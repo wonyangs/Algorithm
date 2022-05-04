ea3602794155318d3f8e3a46f03c89433db7623e
gemstoneyang <bennyws99@gmail.com>
2022-05-04 11:59:44 +0900

    2741.java

---
+++ JAVA/BOJ/2741.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+    public void solution() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i < N+1; ++i) {
+            bw.write(i+"\n");
+        }
+        
+        bw.flush();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
+}
