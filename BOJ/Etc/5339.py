6d60ef70f330aea1155f9f6a0453a550ae9b9cc3
gemstoneyang <bennyws99@gmail.com>
2022-05-25 09:25:05 +0900

    # 5339 콜센터

---
+++ BOJ/Etc/5339.py
@@ -0,0 +1,7 @@
+# Solved on 2022. 5. 25.
+# 5339 콜센터
+
+import sys
+input = sys.stdin.readline
+
+print("     /~\\\n    ( oo|\n    _\\=/_\n   /  _  \\\n  //|/.\\|\\\\\n ||  \\ /  ||\n============\n|          |\n|          |\n|          |")
