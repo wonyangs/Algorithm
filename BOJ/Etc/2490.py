7ec28416761d3522d302e5c6fc699de2dfd46991
Wonseok Yang <43935708+gemstoneyang@users.noreply.github.com>
2022-06-02 13:10:16 +0900

    Create 2490.py

---
+++ BOJ/Etc/2490.py
@@ -0,0 +1,12 @@
+for _ in range(3):
+    a = sum([*map(int, input().split())])
+    if a == 0:
+        print("D")
+    elif a == 3:
+        print("A")
+    elif a == 2:
+        print("B")    
+    elif a == 1:
+        print("C")
+    elif a == 4:
+        print("E")
