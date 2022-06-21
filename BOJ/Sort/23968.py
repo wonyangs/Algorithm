405a525d870a7e17be02653392af0a2bffa9f8d7
gemstoneyang <bennyws99@gmail.com>
2022-06-21 13:39:18 +0900

    # 23968 알고리즘 수업 - 버블 정렬 1

---
+++ BOJ/Sort/23968.py
@@ -0,0 +1,23 @@
+# Solved on 2022. 6. 21.
+# 23968 알고리즘 수업 - 버블 정렬 1
+
+import sys
+input = sys.stdin.readline
+
+
+def bubble_sort(arr):
+    count = 0
+    for last in range(N - 1, 0, -1):
+        for i in range(last):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                count += 1
+                if count == K:
+                    print(arr[i], arr[i + 1])
+                    sys.exit(0)
+    print(-1)
+
+
+N, K = map(int, input().split())
+arr = [*map(int, input().split())]
+bubble_sort(arr)
