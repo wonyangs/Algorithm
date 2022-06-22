e2f9889f8563246da4e8e14259426347732feb68
gemstoneyang <bennyws99@gmail.com>
2022-06-22 11:56:53 +0900

    # 2422 한윤정이 이탈리아에 가서 아이스크림을 사먹는데

--- BOJ/Brute_force/2422.py
+++ BOJ/Brute_force/2422.py
@@ -1,16 +1,15 @@
 # Solved on 2022. 6. 22.
 # 2422 한윤정이 이탈리아에 가서 아이스크림을 사먹는데
 
-from itertools import combinations
 import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-arr = set(i for i in range(1, N+1))
 ice_info = [[*map(int, input().split())] for _ in range(M)]
 result = set()
+com_len = (N * (N-1) * (N-2)) // 6 if N >= 3 else 0
 for a, b in ice_info:
     for i in range(1, N+1):
         if i != a and i != b:
             result.add(tuple(sorted([a, b, i])))
-print(len(list(combinations(arr, 3))) - len(result))
+print(com_len - len(result))
