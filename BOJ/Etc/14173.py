6b455f20b7628899d6b2e7d39f2d34129e899f4e
Wonseok Yang <43935708+gemstoneyang@users.noreply.github.com>
2023-04-28 15:24:10 +0900

    # 14173 Square Pasture

---
+++ BOJ/Etc/14173.py
@@ -0,0 +1,6 @@
+x1, y1, x2, y2 = map(int, input().split())
+x3, y3, x4, y4 = map(int, input().split())
+width = max(x2, x4) - min(x1, x3)
+height = max(y2, y4) - min(y1, y3)
+side = max(width, height)
+print(side * side)
