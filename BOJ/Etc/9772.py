07d42fe8c650cb7c24b5491b0c90725dace99c99
Wonseok Yang <43935708+gemstoneyang@users.noreply.github.com>
2023-06-15 09:01:48 +0900

    # 9772 Quadrants

---
+++ BOJ/Etc/9772.py
@@ -0,0 +1,15 @@
+while True:
+    x, y = map(float, input().split())
+    if x == 0 and y == 0:
+        print('AXIS')
+        break
+    if x == 0 or y == 0:
+        print('AXIS')
+    elif x > 0 and y > 0:
+        print('Q1')
+    elif x < 0 and y > 0:
+        print('Q2')
+    elif x < 0 and y < 0:
+        print('Q3')
+    elif x > 0 and y < 0:
+        print('Q4')
