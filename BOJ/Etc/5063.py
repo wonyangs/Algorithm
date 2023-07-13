7418528ee57ffee286d901185eb78b831853af92
Wonseok Yang <43935708+gemstoneyang@users.noreply.github.com>
2023-07-13 20:23:31 +0900

    # 5063 TGN

---
+++ BOJ/Etc/5063.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    a, b, c = map(int, input().split())
+    if a < b - c:
+        print("advertise")
+    elif a == b - c:
+        print("does not matter")
+    else:
+        print("do not advertise")
