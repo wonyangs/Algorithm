04de1a44ec22d098553a607e4012410237af39d3
Wonseok Yang <43935708+gemstoneyang@users.noreply.github.com>
2023-07-25 15:34:01 +0900

    # 9372 상근이의 여행

---
+++ BOJ/Etc/9372.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    for __ in range(m):
+        a, b = map(int, input().split())
+    print(n-1)
