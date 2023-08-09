3001f852057914c6eb1000c0eecaa09f23204939
Wonseok Yang <43935708+gemstoneyang@users.noreply.github.com>
2023-08-09 10:20:13 +0900

    # 28249 Chili Peppers

---
+++ BOJ/Etc/28249.py
@@ -0,0 +1,12 @@
+pepper = {
+        "Poblano": 1500,
+        "Mirasol": 6000,
+        "Serrano": 15500,
+        "Cayenne": 40000,
+        "Thai": 75000,
+        "Habanero": 125000
+    }
+res = 0
+for _ in range(int(input())):
+    res += pepper[input()]
+print(res)
