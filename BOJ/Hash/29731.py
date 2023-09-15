26899ee162ab8636b37ab99399bb607b41a869c2
Wonseok Yang <43935708+gemstoneyang@users.noreply.github.com>
2023-09-15 22:25:10 +0900

    # 29731 2033년 밈 투표

---
+++ BOJ/Hash/29731.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+s = {
+    "Never gonna give you up",
+"Never gonna let you down",
+"Never gonna run around and desert you",
+"Never gonna make you cry",
+"Never gonna say goodbye",
+"Never gonna tell a lie and hurt you",
+"Never gonna stop"
+}
+
+N = int(input())
+cnt = 0
+for _ in range(N):
+    if input().strip() in s:
+        cnt += 1
+if N != cnt:
+    print("Yes")
+else:
+    print("No")
