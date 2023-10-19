d25563fc65d2b0890d47b6462234d2b21be39d52
Wonseok Yang <43935708+gemstoneyang@users.noreply.github.com>
2023-10-19 21:08:01 +0900

    # 30087 진흥원 세미나

---
+++ BOJ/Etc/30087.py
@@ -0,0 +1,15 @@
+seminar_room = {
+    "Algorithm": "204",
+    "DataAnalysis": "207",
+    "ArtificialIntelligence": "302",
+    "CyberSecurity": "B101",
+    "Network": "303",
+    "Startup": 501,
+    "TestStrategy": 105,
+}
+
+N = int(input())
+
+for _ in range(N):
+    seminar = input().strip()
+    print(seminar_room[seminar])
