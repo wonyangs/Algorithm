56f0868ab62736d288efd36675c69fdb001c4991
Wonseok Yang <43935708+gemstoneyang@users.noreply.github.com>
2023-11-06 14:56:22 +0900

    # 10810 공 넣기

---
+++ BOJ/Etc/10810.py
@@ -0,0 +1,10 @@
+N, M = map(int, input().split())
+basket = [0]*N
+
+for _ in range(M):
+    i, j, k = map(int, input().split())
+    for a in range(i-1, j):
+        basket[a] = k
+
+for ball in basket:
+    print(ball, end=' ')
