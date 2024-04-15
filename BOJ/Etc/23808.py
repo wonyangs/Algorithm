791214e92d43adabac4f17d90b6e1b7eabee3ec4
Wonseok Yang <43935708+wonyangs@users.noreply.github.com>
2024-04-15 11:06:15 +0900

    # 23808 골뱅이 찍기 - ㅂ

---
+++ BOJ/Etc/23808.py
@@ -0,0 +1,13 @@
+N = int(input())
+for _ in range(N * 2):
+    print("@" * N, end='')
+    print("   " * N, end='')
+    print("@" * N)
+for _ in range(N):
+    print("@@@@@"*N)
+for _ in range(N):
+    print("@" * N, end='')
+    print("   " * N, end='')
+    print("@" * N)
+for _ in range(N):
+    print("@@@@@"*N)
