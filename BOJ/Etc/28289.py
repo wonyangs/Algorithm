9b9ea38ad7183469cebe2887f92a30335f80e36b
Wonseok Yang <43935708+wonyangs@users.noreply.github.com>
2024-06-03 12:06:44 +0900

    # 28289 과 조사하기

--- BOJ/Etc/28289.py
+++ BOJ/Etc/28289.py
@@ -1,1 +1,17 @@
+N = int(input())
+arr = [0] * 4
+for _ in range(N):
+    g, c, n = map(int, input().split())
+    if g == 1:
+        arr[-1] += 1
+        continue
+    
+    if c == 1 or c == 2:
+        arr[0] += 1
+    elif c == 3:
+        arr[1] += 1
+    else:
+        arr[2] += 1
 
+for i in range(4):
+    print(arr[i])
