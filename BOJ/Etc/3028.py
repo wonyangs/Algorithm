7ae3d9992d1b5fe2468cb7a097d81a6ddd1478c9
Wonseok Yang <43935708+wonyangs@users.noreply.github.com>
2024-06-11 15:00:59 +0900

    # 3028 창영마을

---
+++ BOJ/Etc/3028.py
@@ -0,0 +1,20 @@
+s = input()
+
+b = 1
+for c in s:
+    if c == 'A':
+        if b == 1:
+            b = 2
+        elif b == 2:
+            b = 1
+    elif c == 'B':
+        if b == 2:
+            b = 3
+        elif b == 3:
+            b = 2
+    else:
+        if b == 1:
+            b = 3
+        elif b == 3:
+            b = 1
+print(b)
