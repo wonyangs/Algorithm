8dbe05389b1056db264756b067c8d4f9bb606a28
Wonseok Yang <bennyws99@gmail.com>
2025-10-09 21:40:03 +0900

    # 18795 이동하기 3

---
+++ BOJ/Etc/18795.py
@@ -0,0 +1,9 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+a = list(map(int, sys.stdin.readline().split()))
+b = list(map(int, sys.stdin.readline().split()))
+
+result = sum(a) + sum(b)
+
+print(result)
