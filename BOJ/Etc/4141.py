7460e32c01d25881047fe01a020ebfc17209a1dd
Wonseok Yang <bennyws99@gmail.com>
2025-12-20 23:12:07 +0900

    # 4141 Numbersrebmun

---
+++ BOJ/Etc/4141.py
@@ -0,0 +1,6 @@
+n = int(input())
+m = {'A':2,'B':2,'C':2,'D':3,'E':3,'F':3,'G':4,'H':4,'I':4,'J':5,'K':5,'L':5,'M':6,'N':6,'O':6,'P':7,'Q':7,'R':7,'S':7,'T':8,'U':8,'V':8,'W':9,'X':9,'Y':9,'Z':9}
+for _ in range(n):
+    s = input().upper()
+    d = ''.join(str(m[c]) for c in s)
+    print('YES' if d == d[::-1] else 'NO')
