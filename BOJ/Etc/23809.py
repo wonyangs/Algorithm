f17cfca9eac805269f471c0901d72fd6a31a11d1
Wonseok Yang <bennyws99@gmail.com>
2026-01-03 11:25:07 +0900

    # 23809 골뱅이 찍기 - 돌아간 ㅈ

---
+++ BOJ/Etc/23809.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+for _ in range(n):
+    print('@'*n + '   '*n + '@'*n)
+for _ in range(n):
+    print('@'*n + '  '*n + '@'*n)
+for _ in range(n):
+    print('@'*(3*n))
+for _ in range(n):
+    print('@'*n + '  '*n + '@'*n)
+for _ in range(n):
+    print('@'*n + '   '*n + '@'*n)
